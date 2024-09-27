1405378ebf25a5de9cebc8794d3fd755f5c98c3f
OomsOoms <tanettleship@gmail.com>
2024-09-27 14:28:40 +0100

    Update vite  config to use environment variables for API domain

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://api.nettleship.net', // should make this an env variable
-        changeOrigin: true,
+export default defineConfig(() => {
+  // eslint-disable-next-line no-undef  
+  const env = loadEnv('', process.cwd(), '')
+
+  return {
+    // vite config
+    //define: {
+    //  __APP_ENV__: JSON.stringify(env.APP_ENV),
+    //}, not sure what this does but ill keep it here for now
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.API_DOMAIN, // should make this an env variable
+          changeOrigin: true,
+          secure: env.NODE_ENV === 'production',
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  }
 })
