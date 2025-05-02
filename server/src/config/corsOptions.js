b92da8131e71af01986b627115ed92efbc94cea7
OomsOoms <tanettleship@gmail.com>
2025-05-02 03:16:45 +0100

    update cors

--- server/src/config/corsOptions.js
+++ server/src/config/corsOptions.js
@@ -1,9 +1,18 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: process.env.FRONTEND_DOMAIN,
+  origin: (origin, callback) => {
+    if (!origin || origin === process.env.FRONTEND_DOMAIN) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200,
+  exposedHeaders: ['Content-Length'],
 };
 
 module.exports = cors(corsOptions);
